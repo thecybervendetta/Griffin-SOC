cad2987d4fa61217ecb27b90a8b8e9a6637608b0
TheCyberVendetta <richieeacey@gmail.com>
2026-07-15 09:55:46 +0100

    Update phish_guard.py

--- phish_guard.py
+++ phish_guard.py
@@ -11,7 +11,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-EMAIL = os.environ.get("EMAIL", "richardovensehi@gmail.com")
+EMAIL = os.environ.get("EMAIL", "your_email_address")
 GMAIL_APP_PASSWORD = os.environ.get("GMAIL_APP_PASSWORD", "") 
 CHECK_INTERVAL = 5 
 
